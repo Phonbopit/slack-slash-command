6c8ee87ce376af6148998b77181039a6b4cde5b7
Chai Phonbopit <phonbopit@gmail.com>
2016-05-21 16:23:51 +0700

    add logic

--- controller.js
+++ controller.js
@@ -14,7 +14,21 @@ module.exports = {
       let payload = request.payload;
       logger(payload, 'payload');
 
-      reply('Ahoy! How are you?');
+      if (payload.token !== process.ENV.SLACK_TOKEN) {
+        return reply('unautorized');
+      }
+
+      if (payload.text === 'private') {
+        return reply(`Ahoy! ${payload.user_name} How are you?`);
+      } else {
+        return reply({
+          response_type: 'in_channel',
+          text: 'Ahoy! ${payload.user_name}',
+          attachments: [
+            { text: 'How are you? ${payload.user_name}' }
+          ]
+        });
+      }
     }
   }
 }
