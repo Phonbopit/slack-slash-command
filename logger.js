208cd4a940b859b8937ed37d047f7bc3342d544c
Chai Phonbopit <phonbopit@gmail.com>
2016-05-21 13:24:51 +0700

    add custom logger

---
+++ logger.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const chalk = require('chalk');
+
+module.exports = (message, name='') => {
+  if (typeof message === 'object') {
+    console.log(chalk.blue('============================'));
+    console.log(chalk.blue(name));
+    console.log(chalk.blue(JSON.stringify(message, undefined, 2)));
+    console.log(chalk.blue('============================'));
+  } else {
+    console.log(chalk.blue('============================'));
+    console.log(chalk.blue(name));
+    console.log(chalk.blue(message));
+    console.log(chalk.blue('============================'));
+  }
+}
